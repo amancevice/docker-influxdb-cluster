1187c3ed0540887ad0b0928ec4718eec6963c02c
Alexander Mancevice <amancevice@cargometrics.com>
2016-02-04 15:18:34 -0500

    updated cluster.sh to work with 0.10.0

--- example/cluster.sh
+++ example/cluster.sh
@@ -7,23 +7,40 @@ docker network create --driver bridge influxdb
 
 echo
 echo "Starting leader node \"ix0\""
-docker run -d --name ix0 -h ix0 --net influxdb -p 8086 -p 8083 \
-    amancevice/influxdb-cluster:$version -hostname ix0:8088
+docker run --detach \
+    --name ix0 \
+    --hostname ix0 \
+    --net influxdb \
+    --publish 8086 \
+    --publish 8083 \
+    --env INFLUX___META___BIND_ADDRESS='"ix0:8088"' \
+    --env INFLUX___META___HTTP_BIND_ADDRESS='"ix0:8091"' \
+    amancevice/influxdb-cluster:$version
 
 echo
 echo "Starting follower node \"ix1\""
 sleep 1
-docker run -d --name ix1 -h ix1 --net influxdb \
-    amancevice/influxdb-cluster:$version -hostname ix1:8088 -join ix0:8088
+docker run --detach \
+    --name ix1 \
+    --hostname ix1 \
+    --net influxdb \
+    --env INFLUX___META___BIND_ADDRESS='"ix1:8088"' \
+    --env INFLUX___META___HTTP_BIND_ADDRESS='"ix1:8091"' \
+    amancevice/influxdb-cluster:$version -join ix0:8091
 
 echo
 echo "Starting follower node \"ix2\""
 sleep 1
-docker run -d --name ix2 -h ix2 --net influxdb \
-    amancevice/influxdb-cluster:$version -hostname ix2:8088 -join ix0:8088,ix1:8088
+docker run --detach \
+    --name ix2 \
+    --hostname ix2 \
+    --net influxdb \
+    --env INFLUX___META___BIND_ADDRESS='"ix2:8088"' \
+    --env INFLUX___META___HTTP_BIND_ADDRESS='"ix2:8091"' \
+    amancevice/influxdb-cluster:$version -join ix0:8091
 
 echo
-echo "Opening InfluxDB CLI (run \"show servers\" to view cluster)"
+echo "SHOW SERVERS:"
 sleep 1
 docker run --rm -it --net influxdb --entrypoint /usr/bin/influx \
-    amancevice/influxdb-cluster:$version -host ix0
+    amancevice/influxdb-cluster:$version -host ix0 -execute "SHOW SERVERS"
