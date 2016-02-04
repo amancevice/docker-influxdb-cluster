461cd50cfa8e72e4b36c95d8ebe8a30c168c0d37
Alexander Mancevice <amancevice@cargometrics.com>
2016-02-04 14:23:19 -0500

    Allow cluster.sh to use version at invocation time

--- example/cluster.sh
+++ example/cluster.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+version=${1:-latest}
+
 echo "Creating docker bridge network \"influxdb\""
 docker network create --driver bridge influxdb
 
 echo
 echo "Starting leader node \"ix0\""
 docker run -d --name ix0 -h ix0 --net influxdb -p 8086 -p 8083 \
-    amancevice/influxdb-cluster -hostname ix0:8088
+    amancevice/influxdb-cluster:$version -hostname ix0:8088
 
 echo
 echo "Starting follower node \"ix1\""
 sleep 1
 docker run -d --name ix1 -h ix1 --net influxdb \
-    amancevice/influxdb-cluster -hostname ix1:8088 -join ix0:8088
+    amancevice/influxdb-cluster:$version -hostname ix1:8088 -join ix0:8088
 
 echo
 echo "Starting follower node \"ix2\""
 sleep 1
 docker run -d --name ix2 -h ix2 --net influxdb \
-    amancevice/influxdb-cluster -hostname ix2:8088 -join ix0:8088,ix1:8088
+    amancevice/influxdb-cluster:$version -hostname ix2:8088 -join ix0:8088,ix1:8088
 
 echo
 echo "Opening InfluxDB CLI (run \"show servers\" to view cluster)"
 sleep 1
 docker run --rm -it --net influxdb --entrypoint /usr/bin/influx \
-    amancevice/influxdb-cluster -host ix0
+    amancevice/influxdb-cluster:$version -host ix0
